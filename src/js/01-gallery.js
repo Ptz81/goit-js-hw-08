afd5ba718784d4ab665447be30a95b453c8e9d98
Taras Papka <taras.papka@protonmail.com>
2023-02-07 13:04:28 +0200

    added files and code from previous task and import modules

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+
 // Change code below this line
+
+
+//знайти
+const gallery = document.querySelector('.gallery')
+//розмітка у змінній
+const newElement = createElem(galleryItems);
+//додати розмітку після галереї
+gallery.insertAdjacentHTML('beforeend', newElement);
+
+//функція рендеру масиву обєктів та створення розмітки
+  function createElem(item){
+    return item.map(({ preview, original, description })=>{
+        return `<li>
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+  }).join('');
+  }
+
+// var lightbox = $('.gallery a').simpleLightbox({
+//     captionsData: 'alt',
+//     captionPosition: 'bottom',
+//     captionDelay: 250
+//  });
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    // captionsData: 'alt',
+    // captionPosition: 'bottom',
+    // captionDelay: 250
+ });
 
 console.log(galleryItems);
