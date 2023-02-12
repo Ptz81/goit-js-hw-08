463d7dcaea95cd7d0ee5e3d2b2f00d8b6ad02d45
Taras Papka <taras.papka@protonmail.com>
2023-02-12 23:13:01 +0200

    function onPlay and method on

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+//функція - запис у локальне сховище на подію play із інтервалом 1000мс
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+//виклик методу on на подію play
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+    player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
