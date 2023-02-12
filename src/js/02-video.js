dd87b49adb47d7b76917ee3bba17bf8baf98dbf8
Taras Papka <taras.papka@protonmail.com>
2023-02-12 23:40:54 +0200

    try to solve mistake with local storage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
 import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 //функція - запис у локальне сховище на подію play із інтервалом 1000мс
 const onPlay = function(data) {
