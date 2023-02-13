fd93f244749e66b3fa78021ad5102325b4ca7f7e
Taras Papka <taras.papka@protonmail.com>
2023-02-13 10:17:39 +0200

    remarks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,10 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
+// знаходимо елемент
 const iframe = document.querySelector('iframe');
+
+//створюємо плеєр
 const player = new Player(iframe);
 
 //функція - запис у локальне сховище на подію play із інтервалом 1000мс
@@ -13,8 +15,11 @@ const onPlay = function(data) {
 //виклик методу on на подію play
 player.on('timeupdate', throttle(onPlay, 1000));
 
+// змінна, яка витягає дані з Локального сховища
 const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
+
+//програвання зі збереженого місця та відловлювання помилок
     player.setCurrentTime(currentTime).then(function(seconds) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
