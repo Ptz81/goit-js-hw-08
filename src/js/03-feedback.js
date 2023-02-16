c8364d4c3944fd01302ac7d461890e434cf3bccf
Taras Papka <taras.papka@protonmail.com>
2023-02-16 23:32:40 +0200

    adjusted function for storing data

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,33 +2,40 @@ import throttle from 'lodash.throttle';
 
 //знайти елементи
 const formElement = document.querySelector('.feedback-form');
-// const currentData = localStorage.getItem('feedback-form-state');
+// const email = formElement[0];
+// const message = formElement[1];
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+// const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+const currentData = {};
+// console.log(currentData)
+
 
 formElement.addEventListener('input', throttle(inputElementStorage, 500))
-
 formElement.addEventListener('submit', handleFormSubmit);
 
 function inputElementStorage(e) {
-localStorage.setItem('feedback-form-state', e.target.name);
+  currentData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentData));
+  
 }
 
-function serializeForm(formNode) {
-  const { elements } = formNode
 
-  const data = Array.from(elements)
-    .filter((item) => !!item.name)
-    .map((element) => {
-      const { name, value } = element
-
-      return { name, value }
-    })
-
-  console.log(data)
-}
-
-// Просимо форму не відправляти данні самостійно та поведінка для submit
 function handleFormSubmit(event) {
+  event.preventDefault();
+  const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  console.log(storedData);
 
-  event.preventDefault()
-  serializeForm(formElement)
+  formElement.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+
 }
+
+// function serializeForm() {
+
+//   if (true) {
+//     currentData.email.value = storedData.email;
+//     currentData.message.value = storedData.message;
+//   }
+// }
+
+
