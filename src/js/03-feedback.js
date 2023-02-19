c3e0b4715ec7084f886563121c6f618a58c7b7ab
Taras Papka <taras.papka@protonmail.com>
2023-02-19 22:00:25 +0200

    added function for saving data

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,40 +2,41 @@ import throttle from 'lodash.throttle';
 
 //знайти елементи
 const formElement = document.querySelector('.feedback-form');
-// const email = formElement[0];
-// const message = formElement[1];
+const email = formElement[0];
+const message = formElement[1];
+
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-// const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
 const currentData = {};
-// console.log(currentData)
 
 
 formElement.addEventListener('input', throttle(inputElementStorage, 500))
 formElement.addEventListener('submit', handleFormSubmit);
 
+reproductionForm();
+
 function inputElementStorage(e) {
   currentData[e.target.name] = e.target.value.trim();
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentData));
-  
+
 }
 
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-  const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  console.log(storedData);
-
-  formElement.reset();
+function handleFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 
 }
 
-// function serializeForm() {
+function reproductionForm() {
 
-//   if (true) {
-//     currentData.email.value = storedData.email;
-//     currentData.message.value = storedData.message;
-//   }
-// }
+  const parseData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (parseData) {
+    email.value = parseData.email || '';;
+    message.value = parseData.message || '';;
+  }
+}
 
 
